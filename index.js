ffe05c5549fd72ab2392021a8a45f1b616d8e720
Crystal <ceceherrera490@gmail.com>
2021-03-16 23:14:51 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const removeEle = document.getElementById("main");
+removeEle.remove();
+
+const newHeader = document.createElement("h1");
+
+document.body.appendChild(newHeader);
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Crystal is the champion";
